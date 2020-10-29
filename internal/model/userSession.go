2fa1afd7dd406502cf86802079d4573292f7d7d8
jenny <jingxue19960921@gmail.com>
2020-10-29 03:44:25 +0000

    fix typo

--- internal/model/userSession.go
+++ internal/model/userSession.go
@@ -6,6 +6,6 @@ import "time"
 type UserSession struct {
 	ID           string    `xorm:"id" json:"id"`
 	UserID       string    `xorm:"user_id" json:"user_id"`
-	LoginTime    time.Time `xorm:"login_time" json:"password,omitempty" r-validate:"password"`
+	LoginTime    time.Time `xorm:"login_time" json:"login_time,omitempty" r-validate:"password"`
 	LastSeenTime time.Time `xorm:"last_seen_time" json:"last_seen_time,omitempty"`
 }
